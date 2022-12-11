0d574ebd74209f8b8e6e9ee9b982bd6d276e4367
Ionut Rosalim <rosalim.sorinionut@gmail.com>
2022-12-11 23:00:14 +0200

    added config file for vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+
+export default () => {
+
+    return defineConfig({
+        plugins: [react()],
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                },
+            },
+        }
+    });
+}
