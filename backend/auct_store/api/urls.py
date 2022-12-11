e7b297a13c194433937c636c40c03de5049aacde
Andoni Laurentiu <iliuta.andoni01@e-uvt.ro>
2022-12-11 22:01:46 +0200

    basic rest

--- backend/auct_store/api/urls.py
+++ backend/auct_store/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("home/", views.ProductView.as_view()),
-    path("", views.ProductView.as_view()),
-    path("products/", views.ProductList.as_view())
+    path("products/<int:id>/delete/", views.ProductDestroy.as_view()),
+    path("products/new/", views.ProductView.as_view()),
+    path("products/", views.ProductList.as_view()),
 
 ]
--- backend/auct_store/api/views.py
+++ backend/auct_store/api/views.py
@@ -1,17 +1,35 @@
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from django.http import JsonResponse
 
 from . import serializers
 from .models import Product
 
 
-class ProductView(generics.CreateAPIView):
+class ProductView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
 
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['id', 'brand']
+    search_fields = ['brand', 'id']
+
+class ProductDestroy(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+
+    def delete(self, request, *args, **kwargs):
+        product_id = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+        print(product_id)
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('product_data{}'.format(product_id))
+        return response
+
+
     
