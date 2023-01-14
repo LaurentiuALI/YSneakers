0fcd6b11dc106b7e7fcbd489b6945bc23328bb81
Andoni Laurentiu <iliuta.andoni01@e-uvt.ro>
2023-01-14 15:55:52 +0200

    minor changes

--- frontend/auct_store/src/components/templates/template.jsx
+++ frontend/auct_store/src/components/templates/template.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import Navbar from "../navbar/navbar";
-import { Layout } from 'antd';
+import { Layout } from "antd";
 import "./template.css";
 const { Header, Footer, Content } = Layout;
 
 const Template = (props) => {
   return (
     <Layout style={{ height: "100vh" }}>
-
       <Navbar />
 
-      <Content style={{ overflow: "scroll" }}>
+      <Content className='parent-container' style={{ overflow: "scroll", border: "1px solid blue" }}>
         {props.children}
       </Content>
 
-      <Footer >YSneakers ©2023 Created by WE</Footer>
+      <Footer>YSneakers ©2023 Created by WE</Footer>
     </Layout>
   );
 };
 
 export default Template;
-
-
