bee45a40b1dfb73f52cad5047ee1e4679d393c69
Laurentiu <91416707+LaurentiuALI@users.noreply.github.com>
2023-01-24 21:03:38 +0200

    Update settings.py

--- backend/auct_store/auct_store/settings.py
+++ backend/auct_store/auct_store/settings.py
@@ -99,8 +99,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'laurentiuandoni2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'albctquwbtkblddm'
+EMAIL_HOST_USER = 'SOME_EMAIL'
+EMAIL_HOST_PASSWORD = 'SOME_PASS'
 
 
 # Password validation
